35b56191acc05daf91886c63a5eced9372b16189
silin.zhao <scofild429@gmail.com>
2022-09-13 15:28:40 +0200

    ready for the presentation

--- goai/myai.go
+++ goai/myai.go
@@ -1,11 +1,11 @@
 package main
 
-import "github.com/scofild429/goai/goai/mpicode"
+import "github.com/scofild429/goai/goai/singlenode"
 
 func main() {
-	// singlenode.Single_node_iris(true)
+	singlenode.Single_node_iris(true)
 	// mpicode.Mpi_iris_Allreduce()
 	// mpicode.Mpi_iris_SendRecv()
 	// mpicode.Mpi_images_Allreduce()
-	mpicode.Mpi_images_SendRecv()
+	// mpicode.Mpi_images_SendRecv()
 }
